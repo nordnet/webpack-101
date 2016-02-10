3b1eb58fb0eb6d21ae9552f35ebbb42998c725e6
Dmitry Demyankov <dmitry.demyankov@gmail.com>
2016-02-11 00:07:33 +0100

    added watch option and generation of source maps

--- webpack.config.js
+++ webpack.config.js
@@ -3,5 +3,13 @@ module.exports = {
   output: {
     filename: 'build.js',
     library: 'app'
-  }
+  },
+
+  watch: true,
+
+  watchOptions: {
+    aggregateTimeout: 100
+  },
+
+  devtool: 'inline-cheap-module-source-map'
 };
